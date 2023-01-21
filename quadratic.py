c7cd94c0db9c713e4eef770e0be41766507d00a4
OvenSausage <78618224+ovensausage@users.noreply.github.com>
2023-01-21 12:18:57 +0000

    Final Iteration

--- quadratic.py
+++ quadratic.py
@@ -1,20 +1,22 @@
-a = int(input("a = "))
-b = int(input("b = "))
-c = int(input("c = "))
+print("Quadratic Equation Calculator")
+while True:
+    a = int(input("a = "))
+    b = int(input("b = "))
+    c = int(input("c = "))
 
-if a == 0:
-    x1 = -1*c/b
-    x2 = "No second value of x "
-elif b == 0:
-    x1 = (c**0.5)
-    x2 = -1*(c**0.5)
-elif c == 0:
-    x1 = (-1*b/a)**(1/2)
-    x2 = -1*(-1*b/a)**(1/2)
-else:
-    dis = b**2-4*a*c
-    x1 = (-b+dis**(1/2))/(2*a)
-    x2 = (-b-dis**(1/2))/(2*a)
-print(x1)
-print(x2)
+    if a == 0:
+        x1 = -1*c/b
+        x2 = "No second value of x "
+    elif b == 0:
+        x1 = (c**0.5)
+        x2 = -1*(c**0.5)
+    elif c == 0:
+        x1 = (-1*b/a)**(1/2)
+        x2 = -1*(-1*b/a)**(1/2)
+    else:
+        dis = b**2-4*a*c
+        x1 = (-b+dis**(1/2))/(2*a)
+        x2 = (-b-dis**(1/2))/(2*a)
+    print(x1)
+    print(x2)
 
