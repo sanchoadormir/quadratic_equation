95cdfff2df0d1c3b4c7db026732a6e797c2ebc0c
OvenSausage <78618224+ovensausage@users.noreply.github.com>
2023-01-21 11:33:40 +0000

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,22 @@
-a = int(input("a = "))
-b = int(input("b = "))
-c = int(input("c = "))
-if a != int:
-    print("a not an int") 
-if b == 0:
-    x1 = (c**0.5)
-    x2 = -1*(c**0.5)
-elif c == 0:
-    x1 = (-1*b/a)**(1/2)
-    x2 = -1*(-1*b/a)**(1/2)
+a = input("a = ")
+b = input("b = ")
+c = input("c = ")
+
+if not a.isnumeric() or not b.isnumeric() or not c.isnumeric():
+    print("Input values must be numbers")
 else:
-    x1 = (-b+(b**2-4*a*c)**(1/2))/(2*a)
-    x2 = (-b-(b**2-4*a*c)**(1/2))/(2*a)
-print(x1)
-print(x2)
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    
+    if a == 0:
+        print("The coefficient of x^2 cannot be zero")
+    else:
+        dis = b**2 - 4*a*c
+        if dis < 0:
+            print("The solutions are complex numbers")
+        else:
+            x1 = (-b + (dis)**(1/2))/(2*a)
+            x2 = (-b - (dis)**(1/2))/(2*a)
+            print("x1 = ", x1)
+            print("x2 = ", x2)
