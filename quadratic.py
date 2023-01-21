948d0932816f0c43cec2f17393dab38c99361dc9
OvenSausage <78618224+ovensausage@users.noreply.github.com>
2023-01-21 12:15:58 +0000

    Final iteration (maybe)

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,20 @@
 a = int(input("a = "))
 b = int(input("b = "))
 c = int(input("c = "))
+
 if a == 0:
-     
-if b == 0:
+    x1 = -1*c/b
+    x2 = "No second value of x "
+elif b == 0:
     x1 = (c**0.5)
     x2 = -1*(c**0.5)
 elif c == 0:
     x1 = (-1*b/a)**(1/2)
     x2 = -1*(-1*b/a)**(1/2)
 else:
-    x1 = (-b+(b**2-4*a*c)**(1/2))/(2*a)
-    x2 = (-b-(b**2-4*a*c)**(1/2))/(2*a)
+    dis = b**2-4*a*c
+    x1 = (-b+dis**(1/2))/(2*a)
+    x2 = (-b-dis**(1/2))/(2*a)
 print(x1)
 print(x2)
+
